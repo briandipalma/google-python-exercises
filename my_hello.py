1add78e2e27f52ce3bf82b403485590e36898dcf
Brian Di Palma <offler@gmail.com>
2015-06-25 13:38:26 +0100

    Add my_hello module

--- my_hello.py
+++ my_hello.py
@@ -8,11 +8,29 @@ Created on 24 Jun 2015
 
 import sys 
 
+def repeat(s, exclaim):
+  """
+  Returns the string 's' repeated 3 times.
+  If exclaim is true, add exclamation marks.
+  """
+  
+  result = s + s + s
+  
+  if exclaim:
+    result += "!!!"
+
+  return result
+
 def main():
-  if len(sys.argv) > 2:
-    print "Hello", sys.argv[1]
+  if len(sys.argv) >= 2:
+    print "Hello,", sys.argv[1]
   else:
     print "Hello, there"
+    print repeat("Hi", True)
+    
+    result = "Test"
+    
+    print result
 
 if __name__ == "__main__":
   main()
